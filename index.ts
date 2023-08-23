fb82e4f00d7d47220a70c3f306e773c0a1380bd1
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-23 22:32:39 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import { from } from 'rxjs';
 import { skip, filter } from 'rxjs/operators';
 
-const numArrayObs = from([1,2,3,4,5,6,7,8,9,10]);
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/skip
+// Example 2: Short hand for a specific filter use case
+const numArrayObs = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 // 3,4,5...
-const skipObs = numArrayObs.pipe(skip(2))
-    .subscribe(console.log);
+const skipObs = numArrayObs.pipe(skip(2)).subscribe(console.log);
 
 // 3,4,5...
-const filterObs = numArrayObs.pipe(
-    filter((val, index) => index > 1)
-  )
+const filterObs = numArrayObs
+  .pipe(filter((val, index) => index > 1))
   .subscribe(console.log);
 
 //Same output!
